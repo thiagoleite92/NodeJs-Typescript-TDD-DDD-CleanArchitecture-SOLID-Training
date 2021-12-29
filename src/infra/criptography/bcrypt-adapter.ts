de170e310801731310fd2a2c1c152a537786127c
Thiago Leite <leite.tjs@gmail.com>
2021-12-29 18:13:48 -0300

    feat: ensure BcrypAdapter return a hash on success

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -8,7 +8,7 @@ export class BcrypterAdapter implements Encrypter {
   }
 
   async encrypt (value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt)
-    return ''
+    const hash = await bcrypt.hash(value, this.salt)
+    return hash
   }
 }
